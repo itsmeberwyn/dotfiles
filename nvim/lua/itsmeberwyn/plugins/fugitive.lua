925d017296e7a2965282fce2fab4a4546449c407
itsmeberwyn <bherfelismenia@gmail.com>
2024-09-22 16:27:09 +0800

    :art: add gitsigns for ui git integration and fugitive for interactive git commands

---
+++ nvim/lua/itsmeberwyn/plugins/fugitive.lua
@@ -0,0 +1,18 @@
+-- Note: you must first go inside the fugitive UI, :G or <leader>gs
+-- s <- to add untracked files to staged files
+-- = <- to show inline diff
+-- dv <- to show split diff
+-- dq <- to exit split diff
+-- u <- to unstage the file
+-- X <- to delete the changes to the file
+-- cc <- to go to the commit page
+-- Branches
+-- :G checkout -b <branch-name>
+return {
+	"tpope/vim-fugitive",
+	config = function()
+		vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+		vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>")
+		vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>")
+	end,
+}
