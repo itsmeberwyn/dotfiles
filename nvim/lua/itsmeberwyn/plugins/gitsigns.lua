4dd11bd9778cbd07bac5a5d589f7f95aa95b6e9f
itsmeberwyn <bherfelismenia@gmail.com>
2024-09-22 16:33:11 +0800

    :art: update code format

--- nvim/lua/itsmeberwyn/plugins/gitsigns.lua
+++ nvim/lua/itsmeberwyn/plugins/gitsigns.lua
@@ -16,12 +16,8 @@ return {
       -- Actions
       map("n", "<leader>hs", gs.stage_hunk, "Stage hunk")
       map("n", "<leader>hr", gs.reset_hunk, "Reset hunk")
-      map("v", "<leader>hs", function()
-        gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-      end, "Stage hunk")
-      map("v", "<leader>hr", function()
-        gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-      end, "Reset hunk")
+      map("v", "<leader>hs", function() gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") }) end, "Stage hunk")
+      map("v", "<leader>hr", function() gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") }) end, "Reset hunk")
 
       map("n", "<leader>hS", gs.stage_buffer, "Stage buffer")
       map("n", "<leader>hR", gs.reset_buffer, "Reset buffer")
@@ -30,15 +26,11 @@ return {
 
       map("n", "<leader>hp", gs.preview_hunk, "Preview hunk")
 
-      map("n", "<leader>hb", function()
-        gs.blame_line({ full = true })
-      end, "Blame line")
+      map("n", "<leader>hb", function() gs.blame_line({ full = true }) end, "Blame line")
       map("n", "<leader>hB", gs.toggle_current_line_blame, "Toggle line blame")
 
       map("n", "<leader>hd", gs.diffthis, "Diff this")
-      map("n", "<leader>hD", function()
-        gs.diffthis("~")
-      end, "Diff this ~")
+      map("n", "<leader>hD", function() gs.diffthis("~") end, "Diff this ~")
 
       -- Text object
       map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "Gitsigns select hunk")
