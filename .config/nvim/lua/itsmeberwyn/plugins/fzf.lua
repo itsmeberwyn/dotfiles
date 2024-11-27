8d9ef841ea88cab5b1989eb7f110d1988e159416
itsmeberwyn <itsmeberwyn@hireme.com>
2024-11-27 13:53:24 +0800

    :fire: remove telescope config

---
+++ .config/nvim/lua/itsmeberwyn/plugins/fzf.lua
@@ -0,0 +1,55 @@
+return {
+  -- Add fzf-lua with optional dependencies
+  {
+    'ibhagwan/fzf-lua',
+    dependencies = { 'nvim-tree/nvim-web-devicons' }, -- Optional for icons
+    keys = {
+      -- Keybindings for fzf-lua
+      { '<leader>ff', function() require('fzf-lua').files() end, desc = 'Find Files' },
+      { '<leader>fg', function() require('fzf-lua').live_grep() end, desc = 'Live Grep' },
+      { '<leader>fb', function() require('fzf-lua').buffers() end, desc = 'Buffers' },
+      { '<leader>fh', function() require('fzf-lua').help_tags() end, desc = 'Help Tags' },
+      { '<leader>fd', function() require('fzf-lua').diagnostics_workspace() end, desc = 'Workspace Diagnostics' },
+    },
+    config = function()
+      require('fzf-lua').setup({
+        winopts = {
+          height = 0.85,
+          width = 0.80,
+          preview = {
+            layout = 'flex',
+            horizontal = 'right:60%',
+            vertical = 'down:45%',
+          },
+        },
+        files = {
+          prompt = 'Files❯ ',
+          cmd = 'fd --type f --hidden --follow --exclude .git',
+          git_icons = true,
+          file_icons = true,
+          color_icons = true,
+        },
+        grep = {
+          prompt = 'Rg❯ ',
+          input_prompt = 'Grep For❯ ',
+          rg_opts = '--hidden --column --line-number --no-heading ' ..
+                    '--color=always --smart-case -g "!{.git,node_modules}/*"',
+        },
+        keymap = {
+          builtin = {
+            ['<F1>'] = 'toggle-help',
+            ['<F2>'] = 'toggle-fullscreen',
+          },
+          fzf = {
+            ['ctrl-q'] = 'select-all+accept',
+            ['ctrl-c'] = 'abort',
+          },
+        },
+        fzf_opts = {
+          ['--layout'] = 'default',
+          ['--info'] = 'inline',
+        },
+      })
+    end,
+  },
+}
