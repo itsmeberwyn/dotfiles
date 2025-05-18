f6dc932419c50eaa10784d54179b6ffc78232bc7
itsmeberwyn <bherfelismenia@gmail.com>
2025-05-19 00:50:27 +0800

    :sparkles: add persistence.nvim

---
+++ .config/nvim/lua/itsmeberwyn/plugins/persistence.lua
@@ -0,0 +1,16 @@
+return {
+  "folke/persistence.nvim",
+  event = "BufReadPre",                                           -- this will only start session saving when an actual file was opened
+  opts = {
+    dir = vim.fn.expand(vim.fn.stdpath("state") .. "/sessions/"), -- directory where session files are saved
+    options = { "buffers", "curdir", "tabpages", "winsize" },     -- sessionoptions used for saving
+    pre_save = nil,                                               -- a function to call before saving the session
+    save_empty = false,                                           -- don't save if there are no open file buffers
+  },
+  keys = {
+    { "<leader>qs", function() require("persistence").load() end,                desc = "load the session for the current directory" },
+    { "<leader>qS", function() require("persistence").select() end,              desc = "Select Session" },
+    { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "load the last session" },
+    { "<leader>qd", function() require("persistence").stop() end,                desc = "stop Persistence => session won't be saved on exit" }
+  }
+}
