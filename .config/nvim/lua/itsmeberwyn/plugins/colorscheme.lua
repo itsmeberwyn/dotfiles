1d213f9b519b7a023484a507796453cf72de5a6b
itsmeberwyn <bherfelismenia@gmail.com>
2025-08-24 04:38:23 +0800

    :art: fix warning message for go_definitions fzf and refactor config on colorscheme

--- .config/nvim/lua/itsmeberwyn/plugins/colorscheme.lua
+++ .config/nvim/lua/itsmeberwyn/plugins/colorscheme.lua
@@ -2,7 +2,17 @@ return {
 	"catppuccin/nvim",
 	name = "catppuccin",
 	priority = 1000,
-	config = function()
-		vim.cmd("colorscheme catppuccin-mocha")
+	opts = {
+		term_colors = true,
+		transparent_background = false,
+		dim_inactive = {
+			enabled = false, -- dims the background color of inactive window
+			shade = "dark",
+			percentage = 0.10, -- percentage of the shade to apply to the inactive window
+		},
+	},
+	config = function(_, opts)
+		require("catppuccin").setup(opts)
+		vim.cmd.colorscheme("catppuccin-macchiato")
 	end,
 }
